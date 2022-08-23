edaf9a7c9422dc86c67889eb868be8ef6e4893ab
nikhilsaharan33 <90635440+nikhilsaharan33@users.noreply.github.com>
2022-08-24 01:14:35 +0530

    Update AstarVisualizer.py

--- AstarVisualizer.py
+++ AstarVisualizer.py
@@ -10,7 +10,7 @@ pg.display.set_caption("A* Path Finding Algorithm")
 # Colors to be used in project
 BLACK = (0, 0, 0)
 PURPLE = (128, 0, 128)
-BLUE = (0, 255, 0)
+BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 ORANGE = (255, 165, 0)
 GREY = (128, 128, 128)
